7742365da3cc386cdfcb3ecc9bbb03c844a5e845
Skyler_Shelvin <skylershelvin@gmail.com>
2023-06-14 18:45:13 -0500

    changes to the client-side web application

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,8 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+ // done to make the prospective view behave like html element
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
 
@@ -32,7 +33,12 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem: document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    elem.setAttribute('view', 'y_line')
+    elem.setAttribute('column-pivots', '["stock"]')
+    elem.setAttribute('row-pivots', '["timestamp"]')
+    elem.setAttribute('columns', '["top_ask_price"]')
+    elem.setAttribute('aggregates', '["stock": "distinct count"]',"top_ask_")
 
     const schema = {
       stock: 'string',
